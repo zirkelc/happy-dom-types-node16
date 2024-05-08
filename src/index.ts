ec45245b2e1da58b4bc39e8f197abaf3740a22b8
zirkelc <chris.zirkel@gmail.com>
2024-05-08 08:38:05 +0200

    first commit

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+import { Window } from 'happy-dom';
+// The current file is a CommonJS module whose imports will produce 'require' calls; 
+// however, the referenced file is an ECMAScript module and cannot be imported with 'require'.
+// Consider writing a dynamic 'import("happy-dom")' call instead.
+// To convert this file to an ECMAScript module, change its file extension to '.mts' or create a local package.json file with `{ "type": "module" }`.
